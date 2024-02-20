b33363f6cb86dd63d05f4ae5b8d0ba9f054b6f5d
Bruck-3 <brucktafesse@yahoo.com>
2024-02-20 19:51:34 +0300

    social-links-profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#e9ff32", // hsl(75, 94%, 57%)
+        white: "#FFFFFF", // hsl(0, 0%, 100%)
+        grey: "#333333", // hsl(0, 0%, 20%)
+        darkGrey: "#1F1F1F", // hsl(0, 0%, 12%)
+        offBlack: "#141414", // hsl(0, 0%, 8%)
+      },
+      fontFamily: {
+        sans: ["Inter", "Sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
