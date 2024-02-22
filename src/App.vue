7abe39ba7ee9219f09c5810f6dbc80250623fbd1
Bruck-3 <brucktafesse@yahoo.com>
2024-02-22 12:04:32 +0300

    changes based on feedback

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,31 @@ const data = [
 </script>
 
 <template>
-  <div class="min-h-[100vh] p-5 flex justify-center items-center font-sans">
+  <!-- Used <main> to wrap the main content instead of <div> -->
+  <main class="min-h-[100vh] p-5 flex justify-center items-center font-sans">
     <div
       class="bg-darkGrey w-[350px] md-[380px] rounded-[10px] pt-[30px]"
     >
     <div class="flex justify-center mb-[20px]">
       <img src="./assets/profile.png" alt="" class="rounded-full h-[70px] w-[70px]">
     </div>
-      <p class="text-white font-bold text-[26px] text-center my-0">
+      <!-- Used <h1> for the main heading -->
+      <h1 class="text-white font-bold text-[26px] text-center my-0">
         Bruck Demissie
-      </p>
-         <p class="font-bold text-[16px] text-center text-green my-0 pt-3">
+      </h1>
+      <!-- Used <h2> for subheadings -->
+      <h2 class="font-bold text-[16px] text-center text-green my-0 pt-3">
         Addis Ababa, Ethiopia
-      </p>
-         <p class="font-normal text-[12px] text-center text-white pt-3">
+      </h2>
+      <p class="font-normal text-[12px] text-center text-white pt-3">
         "Full Stack Developer and avid reader."
       </p>
       <div class="flex flex-col px-[25px] my-[30px]">
-              <a v-for="(link, index) in data" :key="index" :href="link.link" target="_blank" class="w-full h-[45px] bg-grey hover:bg-green  text-[14px] border-0 text-white hover:text-grey font-bold rounded-[7px] my-[7px] cursor-pointer flex justify-center items-center no-underline"> {{ link.name }}</a>
+        <!-- Iterate over the data using v-for -->
+        <a v-for="(link, index) in data" :key="index" :href="link.link" target="_blank" class="w-full h-[45px] bg-grey hover:bg-green text-[14px] border-0 text-white hover:text-grey font-bold rounded-[7px] my-[7px] cursor-pointer flex justify-center items-center no-underline"> {{ link.name }}</a>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style scoped></style>
